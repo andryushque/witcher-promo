c551c6ff210a2c48ca2b52f86eba8c31afefa65c
Andrey Ustinov <andrew.ustinov23@gmail.com>
2020-06-22 19:03:13 +0300

    add responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: '.slide-next',
   },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.header');
+
+menuBtn.addEventListener('click', function () {
+  menu.classList.toggle('header-active');
+  menuBtn.classList.toggle('menu-btn-active');
 });
